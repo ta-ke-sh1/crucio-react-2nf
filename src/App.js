4a49332d4bbbd552d04196ba90b62df204fab3b6
takesh1 <85833065+ta-ke-sh1@users.noreply.github.com>
2022-10-17 11:39:09 +0700

    entrance loading animation for landing page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, useRoutes } from "react-router-dom";
 import React from "react";
-import Crucio from './js/crucio'
+import Crucio from './js/crucio';
 
 const App = () => {
     const routes = useRoutes([
--- src/js/crucio.js
+++ src/js/crucio.js
@@ -2,7 +2,7 @@ import '../css/animation.css'
 import '../css/common.css'
 import '../css/icons.css'
 import '../css/navigation.css'
-import React from "react";
+import React, {useEffect} from "react";
 import Navigation from './widgets/navigation';
 import Landing from './widgets/landing';
 import About from './widgets/about';
@@ -11,8 +11,52 @@ import Team from './widgets/team';
 import Portfolio from './widgets/portfolio';
 import Contact from './widgets/contact';
 import Footer from './widgets/footer';
+import "@fontsource/montserrat";
+import $ from 'jquery';
 
 export default function Crucio(){
+
+   useEffect(() => {
+      $(window).scroll(scrollTrigger);
+      $(window).ready(loadTrigger);
+   });
+
+   var numbersLoad = false
+   var landed = false;
+
+   const loadTrigger = () => {
+      if (!landed) {
+         landed = true;
+         document.getElementById('crucio-headline').classList.add('leftToRight');
+         document.getElementById('crucio-paragraph').classList.add('rightToLeft');
+      }
+   }
+
+   const scrolledToSection = (elem) => {
+      var docViewTop = $(window).scrollTop();
+      var docViewBottom = docViewTop + $(window).height();
+      var elemTop = $(elem).offset().top;
+      var elemBottom = elemTop + $(elem).height();
+      return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+   }
+
+   const scrollTrigger = () => {
+      if (scrolledToSection($('.numbers')) && !numbersLoad) {
+         numbersLoad = true;
+         $('.value').each(function () {
+            $(this).prop('Counter', 0).animate({
+               Counter: $(this).text()
+            }, {
+               duration: 2500,
+               easing: 'swing',
+               step: function (now) {
+                  $(this).text(Math.ceil(now));
+               }
+            });
+         })
+      }
+   }
+
    return(
       <main>
          <Navigation />
